e60915b98b9fc5841ff9665df759769781fe030d
Megan Jensen <spottedeye2@gmail.com>
2025-03-06 13:26:33 -0600

    I think I finally created it properly

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Git and GitHub Practice Lab
+//
+//  Created by Megan Jensen on 3/6/25.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"GitHub Test!"<<endl;
+    
+    return 1;
+}
